8810829e4077f42287c961b7ca493637a7bd3afb
mahmoud-elzorkany <zorkany@users.noreply.github.com>
2018-03-19 17:59:13 +0100

    initial

---
+++ Parallel/game_parallel.c
@@ -0,0 +1,399 @@
+
+/*
+ * Conway's Game of Life
+ *
+ * A. Mucherino
+ *
+ * PPAR, TP5
+ *
+ * Mahmoud Elzorkany
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <mpi.h>
+
+#define MASTER 0
+
+
+int N = 32;
+int itMax = 20;
+
+// allocation only
+unsigned int* allocate()
+{
+   return (unsigned int*)calloc(N*N,sizeof(unsigned int));
+};
+
+// conversion cell location : 2d --> 1d
+// (row by row)
+int code(int x,int y,int dx,int dy)
+{
+   int i = (x + dx)%N;
+   int j = (y + dy)%N;
+   if (i < 0)  i = N + i;
+   if (j < 0)  j = N + j;
+   return i*N + j;
+};
+
+// writing into a cell location 
+void write_cell(int x,int y,unsigned int value,unsigned int *world)
+{
+   int k;
+   k = code(x,y,0,0);
+   world[k] = value;
+};
+
+// random generation
+unsigned int* initialize_random()
+{
+   int x,y;
+   unsigned int cell;
+   unsigned int *world;
+
+   world = allocate();
+   for (x = 0; x < N; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+         if (rand()%5 != 0)
+         {
+            cell = 0;
+         }
+         else if (rand()%2 == 0)
+         {
+            cell = 1;
+         }
+         else
+         {
+            cell = 2;
+         };
+         write_cell(x,y,cell,world);
+      };
+   };
+   return world;
+};
+
+// dummy generation
+unsigned int* initialize_dummy()
+{
+   int x,y;
+   unsigned int *world;
+
+   world = allocate();
+   for (x = 0; x < N; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+         write_cell(x,y,x%3,world);
+      };
+   };
+   return world;
+};
+
+// "glider" generation
+unsigned int* initialize_glider()
+{
+   int x,y,mx,my;
+   unsigned int *world;
+
+   world = allocate();
+   for (x = 0; x < N; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+         write_cell(x,y,0,world);
+      };
+   };
+
+   mx = N/2 - 1;  my = N/2 - 1;
+   x = mx;      y = my + 1;  write_cell(x,y,1,world);
+   x = mx + 1;  y = my + 2;  write_cell(x,y,1,world);
+   x = mx + 2;  y = my;      write_cell(x,y,1,world);
+                y = my + 1;  write_cell(x,y,1,world);
+                y = my + 2;  write_cell(x,y,1,world);
+
+   return world;
+};
+
+// "small exploder" generation
+unsigned int* initialize_small_exploder()
+{
+   int x,y,mx,my;
+   unsigned int *world;
+
+   world = allocate();
+   for (x = 0; x < N; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+         write_cell(x,y,0,world);
+      };
+   };
+
+   mx = N/2 - 2;  my = N/2 - 2;
+   x = mx;      y = my + 1;  write_cell(x,y,2,world);
+   x = mx + 1;  y = my;      write_cell(x,y,2,world);
+                y = my + 1;  write_cell(x,y,2,world);
+                y = my + 2;  write_cell(x,y,2,world);
+   x = mx + 2;  y = my;      write_cell(x,y,2,world);
+                y = my + 2;  write_cell(x,y,2,world);
+   x = mx + 3;  y = my + 1;  write_cell(x,y,2,world);
+
+   return world;
+};
+
+
+// reading a cell
+int read_cell(int x,int y,int dx,int dy,unsigned int *world)
+{
+   int k = code(x,y,dx,dy);
+   return world[k];
+};
+
+// updating counters
+void update(int x,int y,int dx,int dy,unsigned int *world,int *nn,int *n1,int *n2)
+{
+   unsigned int cell = read_cell(x,y,dx,dy,world);
+   if (cell != 0)
+   {
+      (*nn)++;
+      if (cell == 1)
+      {
+         (*n1)++;
+      }
+      else
+      {
+         (*n2)++;
+      };
+   };
+};
+
+// looking around the cell
+void neighbors(int x,int y,unsigned int *world,int *nn,int *n1,int *n2)
+{
+   int dx,dy;
+
+   (*nn) = 0;  (*n1) = 0;  (*n2) = 0;
+
+   // same line
+   dx = -1;  dy = 0;   update(x,y,dx,dy,world,nn,n1,n2);
+   dx = +1;  dy = 0;   update(x,y,dx,dy,world,nn,n1,n2);
+
+   // one line down
+   dx = -1;  dy = +1;  update(x,y,dx,dy,world,nn,n1,n2);
+   dx =  0;  dy = +1;  update(x,y,dx,dy,world,nn,n1,n2);
+   dx = +1;  dy = +1;  update(x,y,dx,dy,world,nn,n1,n2);   
+
+   // one line up
+   dx = -1;  dy = -1;  update(x,y,dx,dy,world,nn,n1,n2);
+   dx =  0;  dy = -1;  update(x,y,dx,dy,world,nn,n1,n2);
+   dx = +1;  dy = -1;  update(x,y,dx,dy,world,nn,n1,n2);
+};
+
+// computing a new generation
+short newgeneration(unsigned int *world1,unsigned int *world2,int xstart,int xend)
+{
+   int x,y;
+   int nn,n1,n2;
+   unsigned int cell;
+   short change = 0;
+
+   // cleaning destination world
+   for (x = 0; x < N; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+         write_cell(x,y,0,world2);
+      };
+   };
+
+   // generating the new world
+   for (x = xstart; x < xend; x++)
+   {
+      for (y = 0; y < N; y++)
+      {
+
+         //read the neighboring cell
+         neighbors (x, y, world1, &nn, &n1, &n2);
+
+         //check if the cell is alive or dead
+          cell = read_cell (x, y, 0, 0, world1);
+
+          //live cell
+          if(cell)
+          {
+            //live on case
+            if(nn==2 || nn==3)
+            {
+               write_cell (x, y, cell, world2);
+            }
+
+            //underpopulation case
+            else if(nn<2)
+            {
+               change++;
+            }
+
+            //overpopulatiob case
+            else if(nn>3)
+            {
+               change++;
+            }
+          }
+
+          //dead cell
+          else{
+            //reproduction case
+            if (nn == 3)
+            {
+               int type;
+               if (n1 > n2)
+                  type = 1;
+               else
+                  type = 2;
+               write_cell (x, y, type, world2);
+               change++;
+            }
+          }
+      };
+   };
+   return change;
+};
+
+// cleaning the screen
+void cls()
+{
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        fprintf(stdout,"\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    }
+}
+
+// diplaying the world
+void print(unsigned int *world)
+{
+   int i;
+   cls();
+   for (i = 0; i < N; i++)  fprintf(stdout,"-");  
+
+   for (i = 0; i < N*N; i++)
+   {
+      if (i%N == 0)  fprintf(stdout,"\n");
+      if (world[i] == 0)  fprintf(stdout," ");
+      if (world[i] == 1)  fprintf(stdout,"o");
+      if (world[i] == 2)  fprintf(stdout,"x");
+   };
+   fprintf(stdout,"\n");
+
+   for (i = 0; i < N; i++)  fprintf(stdout,"-");  
+   fprintf(stdout,"\n");
+   sleep(1);
+};
+
+// main
+int main(int argc,char *argv[])
+{
+
+   int nb_process,lines,rank;
+   int start,end,start_pos,end_pos;
+   int steps=0;
+   int it,change;
+   unsigned int *world1,*world2;
+   unsigned int *worldaux;
+   MPI_Status status;
+   MPI_Request request;
+
+   MPI_Init (&argc, &argv);
+   MPI_Comm_size (MPI_COMM_WORLD, &nb_process);
+
+   // verify that N is divisible by processes number
+   if (N % nb_process != 0)
+   {
+      fprintf (stdout, "Error: number of processors %d is not divisible by N", nb_process);
+      return 2;
+   }
+
+   // number of lines per process
+   lines = N / nb_process;
+
+   //get the current process rank
+   MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+
+//if this is the master process then initialize the world
+if (rank == MASTER)
+   {
+      // generate the initial world
+      //world1 = initialize_dummy();
+      //world1 = initialize_random();
+      //world1 = initialize_glider();
+      world1 = initialize_small_exploder();
+      print (world1);
+
+   }
+   else{
+      world1 = allocate();
+      }
+
+// send initial world to other processes
+   MPI_Bcast (world1, N*N, MPI_INT, MASTER, MPI_COMM_WORLD);
+   
+  //determining the start and the end position for each process
+   start = rank * lines;
+   start_pos = start * N;
+   end = start + lines;
+   end_pos = end * N;
+
+   //allocate the memory for world2
+   world2 = allocate();
+
+
+   //it = 0;  change = 1; 
+
+   do
+   {
+      newgeneration (world1, world2, start, end);
+      worldaux = world1;    world1 = world2;  world2 = worldaux;
+      print (world1);
+
+      
+      //processes exchange the neighbouring rows
+      if (rank != MASTER)
+      {
+         MPI_Isend (&world1[start_pos], N, MPI_INT, rank-1, 0, MPI_COMM_WORLD, &request);
+      }
+      if (rank != nb_process-1)  
+      {
+         MPI_Isend (&world1[end_pos - N], N, MPI_INT, rank+1, 0, MPI_COMM_WORLD, &request);
+      }
+
+      if (rank != MASTER)
+      {
+         MPI_Recv (&world1[start_pos - N], N, MPI_INT, rank-1, 0, MPI_COMM_WORLD, &status);
+      }
+      if (rank != nb_process-1)
+      {
+         MPI_Recv (&world1[end_pos], N, MPI_INT, rank+1, 0, MPI_COMM_WORLD, &status);
+      }
+
+      steps++;
+   }while (steps < itMax);
+
+   MPI_Gather (&world1[start_pos], N * lines, MPI_INT, world2, N * lines, MPI_INT, MASTER, MPI_COMM_WORLD);
+   
+//process 0 prints the final state
+   if (rank == MASTER)
+   {
+      print (world2);
+   }
+
+   // ending
+   free(world1);   free(world2);
+   MPI_Finalize ();
+
+   return 0;
+};
